876f7a2d9b7b9f63441e9cd4adb60d28ca2c696d
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-23 16:28:54 +0200

    Test

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  var checkedAmenities = {};
+  $('input').click(function () {
+    var amenityId = $(this).attr('data-id');
+    var amenityName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    $('div.amenities h4').text($.map(checkedAmenities, function (amenityName) { return amenityName }).join(', '));
+  });
+});
